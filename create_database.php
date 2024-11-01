158c9f64f8a7c32c0848db5e49e47d2b21ee529e
Arnab <arnabdnri@gmail.com>
2024-11-01 19:11:36 +0000

    php script to create tables and populate them with dummy rows

--- create_database.php
+++ create_database.php
@@ -18,14 +18,9 @@ echo "Connected successfully. <br>";
 
 // Drop and create the database
 $sql = "DROP DATABASE IF EXISTS $newDatabase;
-        CREATE DATABASE $newDatabase;
+        CREATE DATABASE $newDatabase
         DEFAULT CHARACTER SET utf8
-        DEFAULT COLLATE utf8_general_ci;
-        GRANT SELECT, UPDATE, INSERT, DELETE
-        ON $newDatabase.*
-        TO 'root'@'localhost'
-        IDENTIFIED BY '';
-        USE $newDatabase;";
+        DEFAULT COLLATE utf8_general_ci;";
 
 if ($mysqli->multi_query($sql)) {
     echo "Database '$newDatabase' created successfully.<br>";
@@ -40,4 +35,120 @@ if ($mysqli->multi_query($sql)) {
 // Select the new database
 $mysqli->select_db($newDatabase);
 
+// Create the 'Items' table if it does not exist
+$sql = "CREATE TABLE IF NOT EXISTS Items
+        (
+        itemID INT AUTO_INCREMENT PRIMARY KEY,
+        itemName VARCHAR(255) NOT NULL,
+        itemDescription VARCHAR(255)
+        )";
+
+if ($mysqli->query($sql) === TRUE) {
+    echo "Table 'Items' created successfully.<br>";
+} else {
+    die("Error creating table: " . $mysqli->error);
+}
+
+// Create the 'Bids' table if it does not exist
+$sql = "CREATE TABLE IF NOT EXISTS Bids
+        (
+        userID INT PRIMARY KEY,
+        auctionID INT NOT NULL,
+        bidAmountGBP INT NOT NULL
+        )";
+
+if ($mysqli->query($sql) === TRUE) {
+    echo "Table 'Bids' created successfully.<br>";
+} else {
+    die("Error creating table: " . $mysqli->error);
+}
+
+// Create the 'Auctions' table if it does not exist
+$sql = "CREATE TABLE IF NOT EXISTS Auctions
+        (
+        auctionID INT AUTO_INCREMENT PRIMARY KEY,
+        auctionDate DATE NOT NULL,
+        startPriceGBP INT NOT NULL,
+        reservePriceGBP INT NOT NULL,
+        quantity INT NOT NULL
+        )";
+
+if ($mysqli->query($sql) === TRUE) {
+    echo "Table 'Auctions' created successfully.<br>";
+} else {
+    die("Error creating table: " . $mysqli->error);
+}
+
+// Create the 'Users' table if it does not exist
+$sql = "CREATE TABLE IF NOT EXISTS Users
+        (
+        userID INT AUTO_INCREMENT PRIMARY KEY,
+        firstName VARCHAR(255) NOT NULL,
+        lastName VARCHAR(255) NOT NULL,
+        dateOfBirth DATE,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        role ENUM('Buyer', 'Seller')
+        )";
+
+if ($mysqli->query($sql) === TRUE) {
+    echo "Table 'Users' created successfully.<br>";
+} else {
+    die("Error creating table: " . $mysqli->error);
+}
+
+// Add sample data to database
+$sql = "INSERT INTO Items (itemID, itemName, itemDescription)
+        VALUES (1, 'Watch', 'Rolex platinum watch made in Switzerland');";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Items (itemID, itemName, itemDescription)
+        VALUES (2, 'Guitar', 'Gibson Les Paul guitar made in 1965 and signed by Jimmy Page. Great for playing the Blues or Rock n Roll.');";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Items (itemID, itemName, itemDescription)
+        VALUES (3, 'Pokemon Card', 'Dragonite cards from 2001 in near-mint condition.');";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Bids (userID, auctionID, bidAmountGBP)
+        VALUES (4, 1, 50000);";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Bids (userID, auctionID, bidAmountGBP)
+        VALUES (5, 2, 2000);";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Auctions (auctionID, auctionDate, startPriceGBP, reservePriceGBP, quantity)
+        VALUES (1, '2024-10-30', 40000, 80000, 1);";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Auctions (auctionID, auctionDate, startPriceGBP, reservePriceGBP, quantity)
+        VALUES (2, '2024-11-01', 1000, 10000, 1);";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Auctions (auctionID, auctionDate, startPriceGBP, reservePriceGBP, quantity)
+        VALUES (3, '2024-11-02', 500, 2000, 2);";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Users (userID, firstName, lastName, dateOfBirth, email, password, role)
+        VALUES (1, 'Leo', 'Messi', '1990-07-23', 'leomessi@gmail.com', 'lmessi', 'Seller');";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Users (userID, firstName, lastName, dateOfBirth, email, password, role)
+        VALUES (2, 'Taylor', 'Swift', '1991-01-25', 'taylorswift@gmail.com', 'tswift', 'Seller');";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Users (userID, firstName, lastName, dateOfBirth, email, password, role)
+        VALUES (3, 'Tom', 'Cruise', '1980-03-03', 'tomcruise@gmail.com', 'tcruise', 'Seller');";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Users (userID, firstName, lastName, dateOfBirth, email, password, role)
+        VALUES (4, 'Chris', 'Martin', '1985-07-11', 'chrismartin@gmail.com', 'cmartin', 'Buyer');";
+$mysqli->query($sql);
+
+$sql = "INSERT INTO Users (userID, firstName, lastName, dateOfBirth, email, password, role)
+        VALUES (5, 'Chris', 'Nolan', '1982-03-01', 'chrisnolan@gmail.com', 'cnolan', 'Buyer');";
+$mysqli->query($sql);
+
+$mysqli->close();
 ?>
